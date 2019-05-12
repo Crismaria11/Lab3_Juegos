63ae96c47fecf4e567381f2da780513c4b69e0cd
Cristina B <bau161260@uvg.edu.gt>
2019-05-11 23:44:41 -0600

    Este si es el verdadero lab que hice

---
+++ Lab#13/Assets/Scripts/ContarMonedas.cs
@@ -0,0 +1,44 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContarMonedas : MonoBehaviour
+{
+    public int puntuacion = 0;
+    public GameObject prefab;
+    public Text txtContador;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Moneda"))
+        {
+            puntuacion += 1;
+            txtContador.text = puntuacion.ToString();
+            Destroy(collision.gameObject);
+        } 
+        if (puntuacion < 3)
+        {
+            if (collision.gameObject.CompareTag("X"))
+            {
+                Destroy(gameObject);
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            GameObject esfera = Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
